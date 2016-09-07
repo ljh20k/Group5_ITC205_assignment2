a3d367d0ff3acc940a708a60206c397e6cd661d0
ljh20k <wngns123>
2016-09-08 03:37:56 +1000

    Ju Hun Lee> StudentUnitRecordMap checkStyle fixed: javadoc comment

--- src/datamanagement/StudentUnitRecordMap.java
+++ src/datamanagement/StudentUnitRecordMap.java
@@ -1,4 +1,8 @@
 package datamanagement;
 
+/**
+ * StudentUnitRecordMap extends HashMap.
+ * Works exactly same as HashMap.
+ */
 public class StudentUnitRecordMap extends java.util.HashMap<String, IStudentUnitRecord> {
 }
