3306e93b098a683955b2e29f5aabb04455ee43fc
kkande01 <kamal_kandel62@yahoo.com>
2016-09-08 09:33:27 +1000

    kamal raj kandel> comments,redundant public modifier etc.fixed

--- src/datamanagement/IStudentUnitRecord.java
+++ src/datamanagement/IStudentUnitRecord.java
@@ -1,22 +1,51 @@
 package datamanagement;
 
+/**
+ * sets the IStudentUnitRecord as an Interface.
+ */
 public interface IStudentUnitRecord {
+    /**
+     * @return student id as integer.
+     */
+     Integer getStudentID();
 
-    public Integer getStudentID();
+    /**
+     * @return unit code as string.
+     */
+     String getUnitCode();
 
-    public String getUnitCode();
+    /**
+     * @param mark is passed to assignment1 as float.
+     */
+    void setAsg1(float mark);
 
-    public void setAsg1(float mark);
+    /**
+     * @return Assignment1 as float value.
+     */
+    float getAsg1();
 
-    public float getAsg1();
+    /**
+     * @param mark is passed to Assignment2.
+     */
+    void setAsg2(float mark);
 
-    public void setAsg2(float mark);
+    /**
+     * @return the Assignment2 as float.
+     */
+    float getAsg2();
 
-    public float getAsg2();
+    /**
+     * @param mark is set for Exam.
+     */
+    void setExam(float mark);
 
-    public void setExam(float mark);
+    /**
+     * @return exam .
+     */
+    float getExam();
 
-    public float getExam();
-
-    public float getTotal();
+    /**
+     * @return total marks .
+     */
+    float getTotal();
 }
