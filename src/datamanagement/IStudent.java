5e1f83db30bce517e153cdd8ac5e57a550cbc3a0
kkande01 <kamal_kandel62@yahoo.com>
2016-09-08 10:14:19 +1000

    kamal raj kandel> redundant public modifier,javadoc comments in IStudent jva class fixed.

--- src/datamanagement/IStudent.java
+++ src/datamanagement/IStudent.java
@@ -1,21 +1,48 @@
 package datamanagement;
 
+/**
+ * Declared IStudent as an interface.
+ */
 public interface IStudent {
+    /**
+     * @return ID as an integer.
+     */
+    Integer getID();
 
-    public Integer getID();
+    /**
+     * @return FirstName as String.
+     */
+    String getFirstName();
 
-    public String getFirstName();
+    /**
+     * @param firstName **FirstName**
+     */
+    void setFirstName(String firstName);
 
-    public void setFirstName(String firstName);
+    /**
+     * @return LastName as String
+     */
+    String getLastName();
 
-    public String getLastName();
+    /**
+     * @param lastName **LastName**
+     */
+    void setLastName(String lastName);
 
-    public void setLastName(String lastName);
+    /**
+     * @param record **IStudentUnitRecord**
+     */
+    void addUnitRecord(IStudentUnitRecord record);
 
-    public void addUnitRecord(IStudentUnitRecord record);
+    /**
+     * @param unitCode **IStudentUnitRecord**
+     * @return UnitRecord.
+     */
+    IStudentUnitRecord getUnitRecord(String unitCode);
 
-    public IStudentUnitRecord getUnitRecord(String unitCode);
-
-    public StudentUnitRecordList getUnitRecords();
+    /**
+     * @return StudentUnitRecordList.
+     */
+    StudentUnitRecordList getUnitRecords();
 
 }
