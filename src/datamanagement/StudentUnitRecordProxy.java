d94affe4f8beec2e89dc0df01f33589f9e615200
ljh20k <wngns123>
2016-09-10 23:51:47 +1000

    Ju Hun Lee> Class UnitProxy checkStyle fixes: param, variables naming convention, Javadoc comments

--- src/datamanagement/StudentUnitRecordProxy.java
+++ src/datamanagement/StudentUnitRecordProxy.java
@@ -1,49 +1,63 @@
 package datamanagement;
 
+/**
+ * Class StudentUnitRecordProxy used to set studentID and unitCode
+ * into StudentUnitRecordManager.
+ */
 public class StudentUnitRecordProxy implements IStudentUnitRecord {
     private Integer studentID;
     private String unitCode;
     private StudentUnitRecordManager mngr;
 
-    public StudentUnitRecordProxy(Integer id, String code) {
+    /**
+     * Constructor to set studentId and unitCode to its' class.
+     * @param id studentId
+     * @param code unitCode
+     */
+    public StudentUnitRecordProxy(final Integer id, final String code) {
         this.studentID = id;
         this.unitCode = code;
         this.mngr = StudentUnitRecordManager.instance();
     }
 
-    public Integer getStudentID() {
+    /**
+     * Getters & Setters.
+     */
+
+    public final Integer getStudentID() {
         return studentID;
     }
 
-    public String getUnitCode() {
+    public final String getUnitCode() {
         return unitCode;
+
     }
 
-    public void setAsg1(float mark) {
+    public final void setAsg1(final float mark) {
         mngr.getStudentUnitRecord(studentID, unitCode).setAsg1(mark);
     }
 
-    public float getAsg1() {
+    public final float getAsg1() {
         return mngr.getStudentUnitRecord(studentID, unitCode).getAsg1();
     }
 
-    public void setAsg2(float mark) {
+    public final void setAsg2(final float mark) {
         mngr.getStudentUnitRecord(studentID, unitCode).setAsg2(mark);
     }
 
-    public float getAsg2() {
+    public final float getAsg2() {
         return mngr.getStudentUnitRecord(studentID, unitCode).getAsg2();
     }
 
-    public void setExam(float mark) {
+    public final void setExam(final float mark) {
         mngr.getStudentUnitRecord(studentID, unitCode).setExam(mark);
     }
 
-    public float getExam() {
+    public final float getExam() {
         return mngr.getStudentUnitRecord(studentID, unitCode).getExam();
     }
 
-    public float getTotal() {
+    public final float getTotal() {
         return mngr.getStudentUnitRecord(studentID, unitCode).getTotal();
     }
 }
